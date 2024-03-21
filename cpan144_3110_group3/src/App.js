56de0976ad982a008fc0a75289c8fb264157f61d
aruzaev <ruzaev.artem21@gmail.com>
2024-03-21 11:57:50 -0400

    Adds basic structure of homepage

--- cpan144_3110_group3/src/App.js
+++ cpan144_3110_group3/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header, Footer, Account, About, Generate } from "./screens";
+import { Navbar } from "./components";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="header_app">
+          <Navbar />
+          <Header />
+        </div>
+        <Switch>
+          <Route path="/generate" component={Generate} />
+          <Route path="/about" component={About} />
+          <Route path="/account" component={Account} />
+          <Route path="/" exact />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
