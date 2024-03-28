21beee952f7fd4fee1711b1d4c816620616bb323
Alex P <afapenha@gmail.com>
2024-03-28 05:02:09 -0400

    Made 'global' loading for unsplash API

--- cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash.js
+++ cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash.js
@@ -1,6 +1,11 @@
+import { Spinner } from "react-bootstrap"
 import axios from "axios";
+import { useState } from "react";
 
 const unsplashClientId = 'wLbZLTACAvzZiBT2ZRSVAyX5yTOO-dc-kg15wcG1AQM';
+let image = null;
+let loading = true;
+
 
 //Returns single image
 const handleSearch = async (searchTerm, setImages) => {
@@ -8,13 +13,26 @@ const handleSearch = async (searchTerm, setImages) => {
     const url = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${unsplashClientId}`;
     try {
         const response = await axios.get(url);
-        console.log(response);
         const randomIndex = Math.floor(Math.random() * response.data.results.length);
-        setImages([response.data.results[randomIndex]]); //gets a random image in the array
+        image = [response.data.results[randomIndex]] //gets a random image in the array
+        console.log(response);
         console.log('All good :)')
+        loading = false;
     } catch (error) {
         console.error(`:( ${error}`);
     }
+    return (
+        <div>
+            {
+                loading ? (
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading....</span>
+                    </Spinner>
+                ) : (
+                    setImages(image) 
+                )
+            }
+        </div>
+    )
 };
-
 export default handleSearch;
