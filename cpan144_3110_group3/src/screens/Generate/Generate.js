388f5571d64e109e45e78ef518eb21a3d8f47055
Alex P <afapenha@gmail.com>
2024-03-28 06:09:05 -0400

    Add search code and proper css for generate

--- cpan144_3110_group3/src/screens/Generate/Generate.js
+++ cpan144_3110_group3/src/screens/Generate/Generate.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import handleSearch from "../../components/Global/unsplashAPI/unsplash";
+import "./Generate.css";
 
 const Generate = () => {
+  const [term, setTerm] = useState('');
+  const [images, setImages] = useState([]);
+
+  const handleButtonClick = () => {
+    handleSearch(term, setImages);
+  };
+
   return (
-    <div>
+    <div className="generate-box-main">
       <h1>Generate</h1>
+      <div className="image-box">
+        {images.map((image) => (
+          <img key={image.id} src={image.urls.regular} alt="random" />
+        ))}
+      </div>
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Search for images..."
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <br />
+        <button onClick={handleButtonClick}>Find</button>
+      </div>
+
     </div>
   );
 };
