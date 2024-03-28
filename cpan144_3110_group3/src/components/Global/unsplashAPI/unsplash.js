b7e84b6f94f632d596b410f2d6576506357c8b2f
Alex P <afapenha@gmail.com>
2024-03-28 06:07:34 -0400

    Add some error handling

--- cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash.js
+++ cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash.js
@@ -1,26 +1,32 @@
 import { Spinner } from "react-bootstrap"
 import axios from "axios";
-import { useState } from "react";
 
 const unsplashClientId = 'wLbZLTACAvzZiBT2ZRSVAyX5yTOO-dc-kg15wcG1AQM';
 let image = null;
 let loading = true;
 
-
 //Returns single image
 const handleSearch = async (searchTerm, setImages) => {
     console.log(searchTerm)
-    const url = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${unsplashClientId}`;
-    try {
-        const response = await axios.get(url);
-        const randomIndex = Math.floor(Math.random() * response.data.results.length);
-        image = [response.data.results[randomIndex]] //gets a random image in the array
-        console.log(response);
-        console.log('All good :)')
+    if (searchTerm.trim() !== '') {
+        const url = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${unsplashClientId}`;
+        try {
+            const response = await axios.get(url);
+            const randomIndex = Math.floor(Math.random() * response.data.results.length);
+            image = [response.data.results[randomIndex]] //gets a random image in the array
+            console.log(response);
+            console.log('All good :)')
+            loading = false;
+        } catch (error) {
+            console.error(`:( ${error}`);
+            loading = false;
+        }
+    } else {
+        console.log('Search term is blank, not making API call.');
         loading = false;
-    } catch (error) {
-        console.error(`:( ${error}`);
+        image = [];
     }
+
     return (
         <div>
             {
@@ -29,7 +35,7 @@ const handleSearch = async (searchTerm, setImages) => {
                         <span className="visually-hidden">Loading....</span>
                     </Spinner>
                 ) : (
-                    setImages(image) 
+                    setImages(image)
                 )
             }
         </div>
