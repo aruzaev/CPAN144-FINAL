c7dc0e73628a7b1ff5bb47aafd823237034817f0
Alex P <afapenha@gmail.com>
2024-03-28 02:42:57 -0400

    Add '<Link>' to Navbar.js allowing it to be an actually navbar

--- cpan144_3110_group3/src/components/Global/NavBarComponents/Navbar/Navbar.js
+++ cpan144_3110_group3/src/components/Global/NavBarComponents/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SearchBar from "../SearchBar/SearchBar";
 import logo from "../../../../assets/logo.png";
-import { DropdownButton, Dropdown } from "react-bootstrap";
 
 const Navbar = () => {
   const [searchActive, setSearchActive] = useState(false);
@@ -17,50 +17,38 @@ const Navbar = () => {
   };
 
   return (
-    // TODO: ADD SMALL SCREEN HAMBURGER MENU BAR
-    // FIX DROPDOWM
-    <div>
-      <nav className="navbar__main">
-        <div className="navbar__left">
-          <div className="navbar__logo">
-            <a href="#home">
-              <img src={logo} alt="logo" />
-            </a>
-          </div>
+    <nav className="navbar__main">
+      <div className="navbar__left">
+        <div className="navbar__logo">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
 
-          <div className="navbar__links-container">
-            <div className="navbar__links-container1">
-              {/* <Dropdown>
-                <Dropdown.Toggle variant="success">Open Menu</Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#">Home Page</Dropdown.Item>
-                  <Dropdown.Item href="#">Settings</Dropdown.Item>
-                  <Dropdown.Item href="#">Logout</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown> */}
-              <a href="#categories">Categories</a>
-            </div>
+        <div className="navbar__links-container">
+          <div className="navbar__links-container1">
+            <Link to="/categories">Categories</Link>
           </div>
         </div>
-        <div className="search__bar-container">
-          <SearchBar
-            onSearch={handleSearch}
-            onToggle={toggleSearchActive}
-            isActive={searchActive}
-          />
+      </div>
+      <div className="search__bar-container">
+        <SearchBar
+          onSearch={handleSearch}
+          onToggle={toggleSearchActive}
+          isActive={searchActive}
+        />
+      </div>
+      <div className="navbar__right">
+        <div className="navbar__links-container2">
+          <Link to="/account">Account</Link>
+          <Link to="/about">About</Link>
         </div>
-        <div className="navbar__right">
-          <div className="navbar__links-container2">
-            <a href="#account">Account</a>
-            <a href="#about">About</a>
-          </div>
-          <div className="navbar__sign">
-            <a href="#signin">Sign in</a>
-            <button type="button">Sign up</button>
-          </div>
+        <div className="navbar__sign">
+          <a href="#signin">Sign in</a>
+          <button type="button">Sign up</button>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
