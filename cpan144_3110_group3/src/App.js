7b5a81f905bb64b34ee62fb8eb196b233f977f6e
Alex P <afapenha@gmail.com>
2024-03-28 02:44:33 -0400

    Add Router and other necessary components to allow navigation

--- cpan144_3110_group3/src/App.js
+++ cpan144_3110_group3/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Header, Home, Footer, Account, About, Generate } from "./screens";
+import { Header, Home, Footer, Account, About, Generate} from "./screens";
+import NotFound from "./screens/NotFound/NotFound";
 import Navbar from "./components/Global/NavBarComponents/Navbar/Navbar";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
+    <Router>
+      <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/generate" element={<Generate />} />
+        </Routes>
+        <Footer />
       </div>
-      <Home />
-      <Account />
-      <About />
-      <Generate />
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
