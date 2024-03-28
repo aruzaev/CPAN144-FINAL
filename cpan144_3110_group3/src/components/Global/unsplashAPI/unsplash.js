0e3324fedeaef86c1004bbd1bc6ecb1a835c5a86
Alex P <afapenha@gmail.com>
2024-03-28 00:02:18 -0400

    Add unsplash api to global components

---
+++ cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const unsplashClientId = 'asdadsM';
+
+//Returns single image
+const handleSearch = async (searchTerm, setImages) => {
+    console.log(searchTerm)
+    const url = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${unsplashClientId}`;
+    try {
+        const response = await axios.get(url);
+        console.log(response);
+        const randomIndex = Math.floor(Math.random() * response.data.results.length);
+        setImages([response.data.results[randomIndex]]); //gets a random image in the array
+        console.log('All good :)')
+    } catch (error) {
+        console.error(`:( ${error}`);
+    }
+};
+
+export default handleSearch;
