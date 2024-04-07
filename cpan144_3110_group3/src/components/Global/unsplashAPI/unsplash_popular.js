559c8ff115cf7945793a71675d4a6f447df18320
Alex P <afapenha@gmail.com>
2024-04-07 06:49:47 -0400

    new file who this

---
+++ cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash_popular.js
@@ -0,0 +1,50 @@
+import { createApi } from "unsplash-js"; //npm i --save unsplash-js
+
+const unsplash = createApi({
+  accessKey: 'wLbZLTACAvzZiBT2ZRSVAyX5yTOO-dc-kg15wcG1AQM',
+});
+
+const handleReqest = async (searchTerm, setImages, orientation) => {
+  let image = null;
+
+  console.log(searchTerm);
+  if (searchTerm.trim() !== '') {
+    try {
+      const response = await unsplash.search.getPhotos({
+        query: searchTerm,
+        orientation: orientation
+      });
+      if (response.errors) {
+        console.log('Error occurred: ', response.errors[0]);
+      } else {
+        const randomIndex = Math.floor(Math.random() * response.response.results.length);
+        image = [response.response.results[randomIndex]]; // gets a random image in the array
+        console.log('All good :)');
+        loading = false;
+      }
+    } catch (error) {
+      console.error(`:( ${error}`);
+      loading = false;
+    }
+  } else {
+    console.log('Search term is blank, not making API call.');
+    loading = false;
+    image = [];
+  }
+
+  return (
+    <div>
+      {
+        loading ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading....</span>
+          </Spinner>
+        ) : (
+          setImages(image)
+        )
+      }
+    </div>
+  );
+};
+
+export default handleReqest;
