3ffbd0416e2d2349148e25b6201cd2232e6bb6c9
Alex P <afapenha@gmail.com>
2024-04-07 06:47:16 -0400

    name change

--- cpan144_3110_group3/src/screens/Generate/Generate.js
+++ cpan144_3110_group3/src/screens/Generate/Generate.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import handleSearch from "../../components/Global/unsplashAPI/unsplash";
+import handleSearch from "../../components/Global/unsplashAPI/unsplash_Generate";
 import {
   Button,
   Form,
