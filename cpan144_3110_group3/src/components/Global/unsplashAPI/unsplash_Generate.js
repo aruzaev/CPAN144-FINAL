f2cdc17036ccae3f08a7318921d30920295c40d0
Alex P <afapenha@gmail.com>
2024-04-07 20:27:33 -0400

    add sessionstoreage code

--- cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash_Generate.js
+++ cpan144_3110_group3/src/components/Global/unsplashAPI/unsplash_Generate.js
@@ -2,7 +2,7 @@ import { Spinner } from "react-bootstrap";
 import { createApi } from "unsplash-js"; //npm i --save unsplash-js
 
 const unsplash = createApi({
-  accessKey: "wLbZLTACAvzZiBT2ZRSVAyX5yTOO-dc-kg15wcG1AQM",
+  accessKey: 'wLbZLTACAvzZiBT2ZRSVAyX5yTOO-dc-kg15wcG1AQM',
 });
 
 const handleSearch = async (searchTerm, setImages, orientation) => {
@@ -10,43 +10,42 @@ const handleSearch = async (searchTerm, setImages, orientation) => {
   let loading = true;
 
   console.log(searchTerm);
-  if (searchTerm.trim() !== "") {
+  if (searchTerm.trim() !== '') {
     try {
       const response = await unsplash.search.getPhotos({
         query: searchTerm,
-        orientation: orientation,
+        perPage: 30,
+        orientation: orientation
       });
       if (response.errors) {
-        console.log("Error occurred: ", response.errors[0]);
+        console.log('Error occurred: ', response.errors[0]);
       } else {
-        const randomIndex = Math.floor(
-          Math.random() * response.response.results.length
-        );
-        image = [response.response.results[randomIndex]]; // gets a random image in the array
-        console.log(image);
-        console.log("All good :)");
+        const randomIndex = Math.floor(Math.random() * response.response.results.length);
+        image = response.response.results[randomIndex]; // gets a random image object in the array
+        console.log('All good :)');
         loading = false;
+
+        // Save the search term and image URL for recent and views
+        const recentSearches = JSON.parse(sessionStorage.getItem('recentSearches')) || [];
+        recentSearches.unshift({ searchTerm, imageUrl: image.urls.regular, image, timestamp: new Date().toISOString() });
+        sessionStorage.setItem('recentSearches', JSON.stringify(recentSearches));
       }
     } catch (error) {
       console.error(`:( ${error}`);
       loading = false;
     }
   } else {
-    console.log("Search term is blank, not making API call.");
+    console.log('Search term is blank, not making API call.');
     loading = false;
     image = [];
   }
 
-  return (
-    <div>
-      {loading ? (
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading....</span>
-        </Spinner>
-      ) : (
-        setImages(image)
-      )}
-    </div>
+  return loading ? (
+    <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading....</span>
+    </Spinner>
+  ) : (
+    setImages([image]) 
   );
 };
 
