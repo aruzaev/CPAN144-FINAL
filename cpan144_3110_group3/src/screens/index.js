28abb038333d26764650f207c6c1e5f3ab8fb148
Alex P <afapenha@gmail.com>
2024-04-07 20:28:05 -0400

    add view

--- cpan144_3110_group3/src/screens/index.js
+++ cpan144_3110_group3/src/screens/index.js
@@ -4,3 +4,4 @@ export { default as Footer } from "./Footer/Footer";
 export { default as Generate } from "./Generate/Generate";
 export { default as Header } from "./Header/Header";
 export { default as Home } from "./Home/Home";
+export { default as View } from "./View/View";
